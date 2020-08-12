0d5e4d3a160afb6c8f090874903bd4f8ea905fa5
Tristan Pacba <tpacba@live.com>
2020-08-12 00:51:51 -0700

    fixed troubleshooting with pushing a null property by adding else statement, created display function to display last search, added reload after every search

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
-$(document.body).ready(function() {
+var weatherStorage = [];
 
-    var weatherStorage = [];
+function initialize() {
+    $("#searchbutton").on("click", function(event) {
+        event.preventDefault();
 
-    $("#day0").text(moment().format('LL'));
-    $("#day1").text(moment().add(1, 'days').format('ddd, MMMM D'));
-    $("#day2").text(moment().add(2, 'days').format('ddd, MMMM D'));
-    $("#day3").text(moment().add(3, 'days').format('ddd, MMMM D'));
-    $("#day4").text(moment().add(4, 'days').format('ddd, MMMM D'));
-    $("#day5").text(moment().add(5, 'days').format('ddd, MMMM D'));
+        var address = $("#searchcity").val();
+        
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+            address: address
+        }, function(results, status) {
+            if (status == "OK") {
+                var city = results[0].formatted_address;
+                var lat = results[0].geometry.location.lat();
+                var lon = results[0].geometry.location.lng();
 
-
-    // var city = "Huntington Beach";
-    // var lat = 33.72001;
-    // var lon = -118.063674;
-
-    // var key = "4d91161064427407dc01f01bcf0ba86c";
-
-    // $.ajax({
-    //     url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly&appid=${key}`,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     response.location = city;
-    //     weatherStorage.push(response);
-    //     localStorage.setItem("cities", JSON.stringify(weatherStorage));
-    // })
+                var key = "4d91161064427407dc01f01bcf0ba86c";
+                $.ajax({
+                    url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly&appid=${key}`,
+                    method: "GET"
+                }).then(function(response) {
+                    response.location = city;
+                    weatherStorage.push(response);
+                    localStorage.setItem("cities", JSON.stringify(weatherStorage));
+                    window.location.reload(false);
+                })
+            } else {
+                alert("Geocode didn't work: " + status);
+            }
+        })
+    })
+}
 
     weatherStorage = JSON.parse(localStorage.getItem("cities"));
     if (weatherStorage != null) {
+
+        $("#day0").text(moment().format('LL'));
+        $("#day1").text(moment().add(1, 'days').format('ddd, MMMM D'));
+        $("#day2").text(moment().add(2, 'days').format('ddd, MMMM D'));
+        $("#day3").text(moment().add(3, 'days').format('ddd, MMMM D'));
+        $("#day4").text(moment().add(4, 'days').format('ddd, MMMM D'));
+        $("#day5").text(moment().add(5, 'days').format('ddd, MMMM D'));
+
         weatherStorage.forEach(function(item, index) {
             var element = `<li class="list-group-item" data-index="${index}">${item.location}</li>`;
             $("#side-city").append(element);
         })
 
-        // $("#main-city").text(weatherStorage[0].location);
-
+        display(weatherStorage.length - 1);
 
         $(document).on("click", "li" , function() {
-            var i = $(this).attr("data-index");
+            var index = $(this).attr("data-index");
+            display(index);
+        })
 
+        function display(i) {
             $("#main-city").text(weatherStorage[i].location);
 
             var fig0 = weatherStorage[i].daily[0].weather[0].icon;
@@ -81,7 +98,7 @@ $(document.body).ready(function() {
             $("#fig5").attr("src", `http://openweathermap.org/img/w/${fig5}.png`)
             $("#temp5").text(weatherStorage[i].daily[5].temp.max);
             $("#humid5").text(weatherStorage[i].daily[5].humidity);
-        })
+        }
+    } else {
+        weatherStorage = [];
     }
-
-})
