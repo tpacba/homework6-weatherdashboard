17c76b3fd3047143a535080fcaacc6107e1e7033
Tristan Pacba <tpacba@live.com>
2020-08-12 01:58:51 -0700

    added loading spinner, added splice method to take off cities if user wants

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var weatherStorage = [];
 function initialize() {
     $("#searchbutton").on("click", function(event) {
         event.preventDefault();
+        $("#spinner").attr("style", "display: block;")
 
         var address = $("#searchcity").val();
         
@@ -11,20 +12,27 @@ function initialize() {
             address: address
         }, function(results, status) {
             if (status == "OK") {
-                var city = results[0].formatted_address;
-                var lat = results[0].geometry.location.lat();
-                var lon = results[0].geometry.location.lng();
+                if (results[0].types[0] == "locality" || results[0].types[0] == "postal_code") {
+                    console.log(results);
+                    var city = results[0].formatted_address;
+                    var lat = results[0].geometry.location.lat();
+                    var lon = results[0].geometry.location.lng();
 
-                var key = "4d91161064427407dc01f01bcf0ba86c";
-                $.ajax({
-                    url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly&appid=${key}`,
-                    method: "GET"
-                }).then(function(response) {
-                    response.location = city;
-                    weatherStorage.push(response);
-                    localStorage.setItem("cities", JSON.stringify(weatherStorage));
+                    var key = "4d91161064427407dc01f01bcf0ba86c";
+                    $.ajax({
+                        url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly&appid=${key}`,
+                        method: "GET"
+                    }).then(function(response) {
+                        response.location = city;
+                        weatherStorage.push(response);
+                        localStorage.setItem("cities", JSON.stringify(weatherStorage));
+                        window.location.reload(false);
+                    })
+                } else {
+                    console.log(results)
+                    alert("Please enter a city.");
                     window.location.reload(false);
-                })
+                }
             } else {
                 alert("Geocode didn't work: " + status);
             }
@@ -34,8 +42,11 @@ function initialize() {
 
     weatherStorage = JSON.parse(localStorage.getItem("cities"));
     if (weatherStorage != null) {
+    if (weatherStorage.length != 0) {
 
-        $("#day0").text(moment().format('LL'));
+        $("#display").attr("style", "margin-top: 20px; display: block;")
+
+        $("#day0").text(moment().format('ddd, MMMM D, YYYY'));
         $("#day1").text(moment().add(1, 'days').format('ddd, MMMM D'));
         $("#day2").text(moment().add(2, 'days').format('ddd, MMMM D'));
         $("#day3").text(moment().add(3, 'days').format('ddd, MMMM D'));
@@ -43,7 +54,7 @@ function initialize() {
         $("#day5").text(moment().add(5, 'days').format('ddd, MMMM D'));
 
         weatherStorage.forEach(function(item, index) {
-            var element = `<li class="list-group-item" data-index="${index}">${item.location}</li>`;
+            var element = `<li class="list-group-item" data-index="${index}">${item.location}<a class="badge float-right exit" data-index="${index}">x</a></li>`;
             $("#side-city").append(element);
         })
 
@@ -54,51 +65,59 @@ function initialize() {
             display(index);
         })
 
-        function display(i) {
-            $("#main-city").text(weatherStorage[i].location);
-
-            var fig0 = weatherStorage[i].daily[0].weather[0].icon;
-            $("#fig0").attr("src", `http://openweathermap.org/img/w/${fig0}.png`)
-
-            $("#temp").text(weatherStorage[i].daily[0].temp.max);
-            $("#humid").text(weatherStorage[i].daily[0].humidity);
-            $("#wind").text(weatherStorage[i].daily[0].wind_speed);
-
-            var uvi = weatherStorage[i].daily[0].uvi
-            $("#uv").text(uvi);
-            if (uvi >= 0 && uvi < 3) {
-                $("#uv").attr("class", "badge badge-success");
-            } else if (uvi >=3 && uvi < 6) {
-                $("#uv").attr("class", "badge badge-warning");
-            } else {
-                $("#uv").attr("class", "badge badge-danger");
-            }
-
-            var fig1 = weatherStorage[i].daily[1].weather[0].icon;
-            $("#fig1").attr("src", `http://openweathermap.org/img/w/${fig1}.png`)
-            $("#temp1").text(weatherStorage[i].daily[1].temp.max);
-            $("#humid1").text(weatherStorage[i].daily[1].humidity);
-
-            var fig2 = weatherStorage[i].daily[2].weather[0].icon;
-            $("#fig2").attr("src", `http://openweathermap.org/img/w/${fig2}.png`)
-            $("#temp2").text(weatherStorage[i].daily[2].temp.max);
-            $("#humid2").text(weatherStorage[i].daily[2].humidity);
-            
-            var fig3 = weatherStorage[i].daily[3].weather[0].icon;
-            $("#fig3").attr("src", `http://openweathermap.org/img/w/${fig3}.png`)
-            $("#temp3").text(weatherStorage[i].daily[3].temp.max);
-            $("#humid3").text(weatherStorage[i].daily[3].humidity);
-
-            var fig4 = weatherStorage[i].daily[4].weather[0].icon;
-            $("#fig4").attr("src", `http://openweathermap.org/img/w/${fig4}.png`)
-            $("#temp4").text(weatherStorage[i].daily[4].temp.max);
-            $("#humid4").text(weatherStorage[i].daily[4].humidity);
-
-            var fig5 = weatherStorage[i].daily[5].weather[0].icon;
-            $("#fig5").attr("src", `http://openweathermap.org/img/w/${fig5}.png`)
-            $("#temp5").text(weatherStorage[i].daily[5].temp.max);
-            $("#humid5").text(weatherStorage[i].daily[5].humidity);
-        }
+        $(document).on("click", ".exit", function() {
+            var index = $(this).attr("data-index");
+            weatherStorage.splice(index, 1);
+            localStorage.setItem("cities", JSON.stringify(weatherStorage));
+            window.location.reload(false);
+        })
+    }
     } else {
         weatherStorage = [];
+    }
+
+    function display(i) {
+        $("#main-city").text(weatherStorage[i].location);
+
+        var fig0 = weatherStorage[i].daily[0].weather[0].icon;
+        $("#fig0").attr("src", `http://openweathermap.org/img/w/${fig0}.png`)
+
+        $("#temp").text(weatherStorage[i].daily[0].temp.max);
+        $("#humid").text(weatherStorage[i].daily[0].humidity);
+        $("#wind").text(weatherStorage[i].daily[0].wind_speed);
+
+        var uvi = weatherStorage[i].daily[0].uvi
+        $("#uv").text(uvi);
+        if (uvi >= 0 && uvi < 3) {
+            $("#uv").attr("class", "badge badge-success");
+        } else if (uvi >=3 && uvi < 6) {
+            $("#uv").attr("class", "badge badge-warning");
+        } else {
+            $("#uv").attr("class", "badge badge-danger");
+        }
+
+        var fig1 = weatherStorage[i].daily[1].weather[0].icon;
+        $("#fig1").attr("src", `http://openweathermap.org/img/w/${fig1}.png`)
+        $("#temp1").text(weatherStorage[i].daily[1].temp.max);
+        $("#humid1").text(weatherStorage[i].daily[1].humidity);
+
+        var fig2 = weatherStorage[i].daily[2].weather[0].icon;
+        $("#fig2").attr("src", `http://openweathermap.org/img/w/${fig2}.png`)
+        $("#temp2").text(weatherStorage[i].daily[2].temp.max);
+        $("#humid2").text(weatherStorage[i].daily[2].humidity);
+        
+        var fig3 = weatherStorage[i].daily[3].weather[0].icon;
+        $("#fig3").attr("src", `http://openweathermap.org/img/w/${fig3}.png`)
+        $("#temp3").text(weatherStorage[i].daily[3].temp.max);
+        $("#humid3").text(weatherStorage[i].daily[3].humidity);
+
+        var fig4 = weatherStorage[i].daily[4].weather[0].icon;
+        $("#fig4").attr("src", `http://openweathermap.org/img/w/${fig4}.png`)
+        $("#temp4").text(weatherStorage[i].daily[4].temp.max);
+        $("#humid4").text(weatherStorage[i].daily[4].humidity);
+
+        var fig5 = weatherStorage[i].daily[5].weather[0].icon;
+        $("#fig5").attr("src", `http://openweathermap.org/img/w/${fig5}.png`)
+        $("#temp5").text(weatherStorage[i].daily[5].temp.max);
+        $("#humid5").text(weatherStorage[i].daily[5].humidity);
     }
